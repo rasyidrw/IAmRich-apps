b326808194904da381eba7e692e5cad79a938a48
rasyidrw <rasyidrespati.30@gmail.com>
2019-10-15 23:18:52 +0700

    another another commit

--- I Am Rich/ViewController.swift
+++ I Am Rich/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("just code on another one?")
+        print("just code on another one")
     }
 
 
