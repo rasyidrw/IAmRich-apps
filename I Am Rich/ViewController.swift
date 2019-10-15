4d52ad0f4f4d92acddfc2042dc926a0abf29059a
rasyidrw <rasyidrespati.30@gmail.com>
2019-10-15 22:30:56 +0700

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Rasyid Respati Wiriaatmaja on 15/10/19.
+//  Copyright © 2019 rasyidrw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
